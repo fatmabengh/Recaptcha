71fe97b2a58da108aca8ee21476681faf039427d
fatma ben ghanem <fatma.benghanem@etudiant-isi.utm.tn>
2019-02-11 10:48:05 +0100

    first commit

---
+++ RecaptchaCompilerPass.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bghanem
+ * Date: 07/02/2019
+ * Time: 11:28
+ */
+
+namespace Grafikart\RecaptchaBundle;
+
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class RecaptchaCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if ($container -> hasParameter('twig.form.resources')){
+            $resources = $container -> getParameter('twig.form.resources') ? : [];
+            @array_unshift($resources,'@Recaptcha/fields.html.twig');
+            $container -> setParameter('twig.form.resources',$resources);
+        }
+    }
+
+
+}
